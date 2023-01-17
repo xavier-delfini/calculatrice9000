49416caf1de8f06f5a085eb22272a6b007f7a7af
xavier-delfini <91735539+xavier-delfini@users.noreply.github.com>
2023-01-17 09:58:06 +0100

    Finalisation de la fonction gestion entrées

--- main.py
+++ main.py
@@ -1,65 +1,94 @@
 from tkinter import *
 from tkinter import ttk
 
+
 def input_process(enter):
     try:
         input_process.storage
-        input_process.number
-        input_process.expression
-        input_process.result
-        input_process.count
-        input_process.sign
 
     except Exception:
-        input_process.storage=[]
-        input_process.number=""
-        input_process.expression=""
-        input_process.result=0
-        input_process.count=0
-        input_process.sign="+"
+        input_process.storage = []
+        input_process.number = ""
+        input_process.expression = ""
+        input_process.result = 0
+        input_process.count = 0
+        input_process.sign = "+"
+        #Si l'entrée est un nombre
     if isinstance(enter, int) or enter == "," or enter == "+/-":
-        enter=str(enter)
+        enter = str(enter)
         if enter == ",": enter = "."
         if enter == "+/-":
-            if input_process.sign == "+": sign.set("-")
+            if input_process.sign == "+":
+                input_process.sign = "-"
+                sign.set("-")
             else:
                 sign.set("")
+                input_process.sign = "+"
         else:
             input_process.number = input_process.number + enter
-            input_process.expression = input_process.expression + enter
-            value.set(input_process.expression)
+            current_number.set(input_process.number)
 
+    # Si l'entrée est "="
     elif enter == "=":
         pass
+
+    # Si l'entrée est "CE"
+    elif enter == "CE":
+        input_process.expression = ""
+        input_process.number = ""
+        value.set(input_process.expression)
+        current_number.set(input_process.number)
+        input_process.storage = []
+
+    # Si l'entrée est une chaine de caractère autre que testé précédemment, ces entrée correspondent a un opérateur
     elif isinstance(enter, str):
-        #Stockage du nombre dans la liste storage
-        input_process.number=int(input_process.number)
-        if sign.get =="-":
-            input_process.number=input_process.number * -1
+        #Le nombre entrer est complet nous pouvont donc le convertir en int ou en float si le int retourne une erreur a cause de la virgule
+        dumper=input_process.number
+        try:
+            input_process.number = int(input_process.number)
+        except Exception:
+            input_process.number = float(input_process.number)
+
+        if input_process.sign == "-" :
+            negative = str(input_process.number)
+            #Si le nombre est le premier alors celui ci ne prendra pas de parentèses
+            if input_process.expression !="": negative = "(-"+negative+")"
+            else: negative = "-"+negative
+            input_process.number = input_process.number * -1
+            print(input_process.expression)
+            input_process.expression = input_process.expression + negative + enter
+            input_process.sign = "+"
+        else:
+
+            input_process.expression = input_process.expression+ dumper + enter
+
         input_process.storage.append(input_process.number)
-        input_process.number=""
+        sign.set("")
+
+        input_process.number = ""
+        current_number.set(input_process.number)
         input_process.storage.append(enter)
         print(input_process.storage)
-        input_process.expression= input_process.expression + enter
-        print (input_process.expression)
+        print(input_process.expression)
         value.set(input_process.expression)
-def sign_number(number,sign_selected):
-    number = int(number)
-    if sign_selected == "-":
-        number = number * -1
-    return number
+        print(value.get())
 
-#def egal(array):
+def egal(operation):
+    calculs=True
+    while calculs=True:
 
 calc = Tk()
 calc.title("Calculatrice")
 
-# Création des deux cadres utilisé dans notre calculatrice
-#Cadre affichage permettant l'affichage du résultat ou de l'opération
-affichage = ttk.Frame(calc, padding="10 10 5 5")
-affichage.grid(column=0, row=0, sticky=(N, E))
-affichage.columnconfigure(0, weight=1)
-affichage.rowconfigure(0, weight=1)
+# Création des trois cadres utilisés dans notre calculatrice
+
+# Cadre display permettant l'affichage de l'opération
+display = ttk.Frame(calc, padding="10 10 5 5")
+display.grid(column=0, row=0, sticky=("N", "E"))
+display.columnconfigure(0, weight=1)
+display.rowconfigure(0, weight=1)
+
+
 
 boutons = ttk.Frame(calc, padding="1 1 1 1")
 boutons.grid(column=0, row=1, sticky=(W, E, S))
@@ -67,9 +96,10 @@ calc.columnconfigure(0, weight=1)
 calc.rowconfigure(0, weight=1)
 value = StringVar()
 sign = StringVar()
-
-ttk.Label(affichage, textvariable=sign).grid(column=0, row=0)
-ttk.Label(affichage, textvariable=value).grid(column=1, row=0)
+current_number = StringVar()
+ttk.Label(display, textvariable=sign).grid(column=0, row=1)
+ttk.Label(display, textvariable=value,font=12).grid(column=0, row=0)
+ttk.Label(display, textvariable=current_number).grid(column=1, row=1)
 ttk.Button(boutons, text="7", command=lambda: input_process(7)).grid(column=0, row=0)
 ttk.Button(boutons, text="8", command=lambda: input_process(8)).grid(column=1, row=0)
 ttk.Button(boutons, text="9", command=lambda: input_process(9)).grid(column=2, row=0)
@@ -87,4 +117,6 @@ ttk.Button(boutons, text="0", command=lambda: input_process(0)).grid(column=1, r
 ttk.Button(boutons, text=",", command=lambda: input_process(",")).grid(column=2, row=3)
 ttk.Button(boutons, text="+", command=lambda: input_process("+")).grid(column=3, row=3)
 ttk.Button(boutons, text="=", command=lambda: input_process("=")).grid(column=4, row=4)
+ttk.Button(boutons, text="CE", command=lambda: input_process("CE")).grid(column=4, row=0)
+
 calc.mainloop()
